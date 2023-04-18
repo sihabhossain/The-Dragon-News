8518791ee1ab8ee64af3d48679d8b8f497422c87
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-18 18:09:32 +0600

    50% work done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -8,7 +8,7 @@ import Marquee from "react-fast-marquee";
 const Header = () => {
   return (
     <Container>
-      <div className="text-center">
+      <div className="text-center mt-5">
         <img src={logo} alt="" />
         <p className="text-secondary">
           <small>Journalism Without Fear or Favor</small>
@@ -31,37 +31,28 @@ const Header = () => {
 
       {/* NAVBAR CODE */}
 
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className="mb-3 mt-3">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#pricing">Career</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Profile</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  <button type="button" class="btn btn-secondary">
+                    Login
+                  </button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </Container>
   );
 };
--- src/Pages/Shared/Rightnav/Rightnav.jsx
+++ src/Pages/Shared/Rightnav/Rightnav.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+import {
+  FaGithub,
+  FaGoogle,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from "../../../assets/bg.png";
 
 const Rightnav = () => {
   return (
-    <div>
-      <h2>this is right nav</h2>
+    <div className="mt-5">
+      <h4>Login With</h4>
+
+      <button type="button" class="btn btn-outline-primary mb-2">
+        <FaGoogle /> Login With Google
+      </button>
+      <button type="button" class="btn btn-outline-secondary">
+        <FaGithub /> Login With Github
+      </button>
+
+      <div className="mt-5">
+        <h4>Find Us On</h4>
+
+        <ListGroup>
+          <ListGroup.Item>
+            {" "}
+            <FaFacebook /> FaceBook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {" "}
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {" "}
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img className="text-center  w-100 rounded mt-3" src={bg} alt="" />
+        <h5 className=" ">Create an Amazing Newspaper</h5>
+        <p className="">
+          Discover thousands of options, easy to customize layouts, one-click to
+          import demo and much more.
+        </p>
+      </div>
     </div>
   );
 };
