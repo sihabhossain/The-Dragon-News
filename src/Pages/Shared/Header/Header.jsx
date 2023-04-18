a77c499625a4fac5530b9efed2678457959ccf76
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-18 17:26:36 +0600

    header done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
+import { Button } from "bootstrap";
+import { Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
@@ -12,7 +15,20 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+
+      <div className="d-flex">
+        <button type="button" className="btn btn-danger">
+          Latest
+        </button>
+        <Marquee className="text-danger" speed={80}>
+          Match Highlights: Germany vs Spain — as it happened ! Match
+          Highlights: Germany vs Spain as. Match Highlights: Brazil vs Argentina
+          — as it happened ! Match Highlights: Brazil vs Argentina as. Match
+          Highlights: Germany vs Spain — as it happened ! Match Highlights:
+          Germany vs Spain as they lost.
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
