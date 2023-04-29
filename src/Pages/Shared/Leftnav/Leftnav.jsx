105dcb11c60c34cee0719feb5938efe238f39933
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-30 00:09:09 +0600

    all done

--- src/Pages/Shared/Leftnav/Leftnav.jsx
+++ src/Pages/Shared/Leftnav/Leftnav.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/categories')
-            .then(res => res.json())
-            .then(data => setCategories(data))
-            .catch(error => console.error(error))
-    }, [])
+  useEffect(() => {
+    fetch("https://dragon-news-server-sihabhossain.vercel.app/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
 
-    return (
-        <div>
-            <h4>All Category</h4>
-            <div className='ps-4'>
-                {
-                    categories.map(category => <p
-                        key={category.id}
-                    >
-                        <Link to={`/category/${category.id}`} className='text-black text-decoration-none'>{category.name}</Link>
-                    </p>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h4>All Category</h4>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/category/${category.id}`}
+              className="text-black text-decoration-none"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -41,7 +41,9 @@ const router = createBrowserRouter([
         path: ":id",
         element: <Category></Category>,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/categories/${params.id}`),
+          fetch(
+            `https://dragon-news-server-sihabhossain.vercel.app/categories/${params.id}`
+          ),
       },
     ],
   },
@@ -57,7 +59,9 @@ const router = createBrowserRouter([
           </PrivateRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/news/${params.id}`),
+          fetch(
+            `https://dragon-news-server-sihabhossain.vercel.app/news/${params.id}`
+          ),
       },
     ],
   },
